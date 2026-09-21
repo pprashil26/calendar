da0eb49a1e7c248dd47d6fab95f6d13783b7b1ce
pprashil26 <prashildigital@gmail.com>
2026-09-21 12:32:25 +1000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='life-calendar-v1.3.1-mobile-form-stability';
+const CACHE='life-calendar-v1.3.2-modal-scroll-lock';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
